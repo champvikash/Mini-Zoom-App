d93bfca87fc1ce808af306949311a85b7dac700b
welcome <champvikash4@gmail.com>
2025-01-21 12:52:33 +0530

    Stop tracking MyFile.kt and add it to .gitignore

--- app/src/main/java/com/example/minizoomapp/Constant.kt
+++
@@ -1,9 +0,0 @@
-package com.example.minizoomapp
-
-object Constant {
-
-    val appId = "ad42da4a4d8d48d8ab16b0308abca45c"
-    val token =
-        "007eJxTYOjjWXIr/tHcvFN/uSvulFy8Ov/5pPLdgvVHuLsLncxOXFmiwJCYYmKUkmiSaJJikWJikWKRmGRolmRgbABkJCeamCazLO5JbwhkZKhZspmZkQECQXxuhtzMvMyq/PzcxIICBgYAOhIlgw=="
-
-}
